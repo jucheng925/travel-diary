7d6ed57db84ca4e95cc042d03b131ba407f3bc35
Julie Cheng <jucheng925@gmail.com>
2024-04-22 10:36:43 -0400

    initial commit

---
+++ server/migrations/versions/68343bf027db_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 68343bf027db
+Revises: 
+Create Date: 2024-04-22 10:36:32.836384
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '68343bf027db'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
