a943036a36a14665f858c420b720866629c50126
Julie Cheng <jucheng925@gmail.com>
2024-05-10 19:14:43 -0400

    install styled material mui

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,31 +1,34 @@
 import React, {useContext} from 'react'
 import { NavLink } from 'react-router-dom'
 import { UserContext } from '../context/UserContext'
+import { Toolbar, AppBar } from '@mui/material';
 
 const Navbar = () => {
   const {currentUser, logout} = useContext(UserContext)
 
   
   return (
-    <nav>
-      <NavLink to= "/" className="nav-link">
-        Home
-      </NavLink>
-      {currentUser ? 
-        <>
-          <NavLink className="nav-link"to= "/profile/edit"> 
-              Edit Profile
-          </NavLink>
-          <NavLink className="nav-link"to="/trips/add"> 
-              Add a trip
-          </NavLink>
-          <NavLink to= "/logout" className="nav-link" onClick={logout}>
-              Logout
-          </NavLink>
+    <AppBar position='sticky'>
+      <Toolbar>
+        <NavLink to= "/" className="nav-link">
+          Home
+        </NavLink>
+        {currentUser ? 
+          <>
+            <NavLink className="nav-link"to= "/profile/edit"> 
+                Edit Profile
+            </NavLink>
+            <NavLink className="nav-link"to="/trips/add"> 
+                Add a trip
+            </NavLink>
+            <NavLink to= "/logout" className="nav-link" onClick={logout}>
+                Logout
+            </NavLink>
 
-        </>
-        : null }
-    </nav>
+          </>
+          : null }
+        </Toolbar>
+    </AppBar>
   )
 }
 
