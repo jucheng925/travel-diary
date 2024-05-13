68c171488cf588f029aea25b3fd5f5df4e154575
Julie Cheng <jucheng925@gmail.com>
2024-05-13 11:18:23 -0400

    nav bar syled completed

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { NavLink } from 'react-router-dom'
 import { UserContext } from '../context/UserContext'
-import { Toolbar, AppBar, styled, Typography, Box, Avatar } from '@mui/material';
+import { Toolbar, AppBar, styled, Typography, Box, Avatar, Menu, MenuItem } from '@mui/material';
+import AddLocationIcon from '@mui/icons-material/AddLocation';
 import "../styled/app.css"
 import { Cloudinary } from '@cloudinary/url-gen';
-import { AdvancedImage } from '@cloudinary/react';
 import { thumbnail } from '@cloudinary/url-gen/actions/resize';
 import {focusOn} from "@cloudinary/url-gen/qualifiers/gravity";
 import {FocusOn} from "@cloudinary/url-gen/qualifiers/focusOn";
@@ -12,6 +12,7 @@ import {FocusOn} from "@cloudinary/url-gen/qualifiers/focusOn";
 
 const Navbar = () => {
   const {currentUser, logout} = useContext(UserContext)
+  const [open, setOpen] = useState(false)
 
   const cld = new Cloudinary({
     cloud: {
@@ -20,10 +21,11 @@ const Navbar = () => {
   });
 
   const AvatarPic = currentUser ? cld.image(currentUser.profile_pic).resize(thumbnail().width(50).height(50).zoom(0.85).gravity(focusOn(FocusOn.face()))) : null
-  AvatarPic ? console.log(AvatarPic.toURL()): null
+
   const StyledToolbar = styled(Toolbar) ({
     display: "flex",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    padding: "10px"
   })
 
   const Icons = styled(Box)(({theme}) => ({
@@ -55,34 +57,70 @@ const Navbar = () => {
   return (
     <AppBar position='sticky'>
       <StyledToolbar>
-        <Typography variant="h6">
-          <NavLink to= "/" style = {activeState} className="nav-link">
-            WanderLog
-          </NavLink>
-        </Typography>
-        <Icons>
-          {currentUser ? 
-            <>
-              <Avatar 
-                alt={currentUser.username}
-                src={AvatarPic.toURL()} />
-                  
-                {/* <AdvancedImage cldImg={AvatarPic} /> 
-              </Avatar> */}
-              <NavLink className="nav-link"to= "/profile/edit" style = {activeState}> 
-                  Edit Profile
-              </NavLink>
+      <Typography variant="h5" sx={{ fontSize: { xs: "small", sm: "large" } }}>
+        <NavLink to= "/" style = {activeState} className="nav-link">
+          WanderLog
+        </NavLink>
+      </Typography>
+        {currentUser ? 
+          <>
+            <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
               <NavLink className="nav-link"to="/trips/add" style = {activeState}> 
-                  Add a trip
+                <AddLocationIcon fontSize='35px'/> Add a trip
               </NavLink>
-              <NavLink to= "/logout" className="nav-link" onClick={logout} style = {activeState}>
-                  Logout
-              </NavLink>
-
+            </Typography>
+        <Icons>
+            <Avatar 
+              alt={currentUser.username}
+              src={AvatarPic.toURL()} 
+              sx={{ bgcolor: '#97ead2'}}/>
+            <NavLink className="nav-link"to= "/profile/edit" style = {activeState}> 
+                Edit Profile
+            </NavLink>
+            <NavLink to= "/logout" className="nav-link" onClick={logout} style = {activeState}>
+              Logout
+            </NavLink>
+        </Icons>
+        <UserBox onClick={(e) => setOpen(true)}>
+          <Avatar 
+            alt={currentUser.username}
+            src={AvatarPic.toURL()} 
+            sx={{ bgcolor: '#97ead2' }}/>
+          {currentUser.username}
+        </UserBox>
             </>
             : null }
-        </Icons>
-        </StyledToolbar>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>
+          <NavLink className="nav-link-small"to="/trips/add" > 
+            Add a trip
+          </NavLink>
+        </MenuItem>
+        <MenuItem>
+          <NavLink className="nav-link-small"to= "/profile/edit"> 
+            Edit Profile
+          </NavLink>
+        </MenuItem>
+        <MenuItem>
+          <NavLink to= "/logout" className="nav-link-small" onClick={logout} >
+              Logout
+          </NavLink>
+        </MenuItem>
+      </Menu>
     </AppBar>
   )
 }
